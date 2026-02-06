85b431502e61938e5432360797947b1921b412d1
Rohan Khandare <rohankhandare429@gmail.com>
2026-02-07 03:14:25 +0530

    signin signup with hashing

---
+++ models/user.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
+
+const userSchema = new Schema(
+  {
+    fullName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    salt: { type: String },
+    password: { type: String, required: true },
+    profileImageURL: {
+      type: String,
+      default: "./public/images/default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function () {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString("hex");
+
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  user.salt = salt;
+  user.password = hashedPassword;
+});
+
+userSchema.static('matchPassword', async function(email, password){
+    const user = await this.findOne({email})
+    if(!user) throw new Error('user not found');
+
+    const salt = user.salt;
+    const hashedPassword= user.password;
+
+    const userProvidedHash = createHmac('sha256', salt).update(password).digest("hex");
+
+    if(hashedPassword !== userProvidedHash) throw new Error('incorrect password');
+
+    return {...user, password:undefined, salt:undefined};
+} )
+
+const User = model("user", userSchema);
+
+module.exports = User;
---
+++ routes/user.js
@@ -0,0 +1,32 @@
+const {Router}  = require('express')
+const router = Router();
+const User = require('../models/user')
+
+router.get('/signin', (req, res)=>{
+    return res.render('signin')
+})
+
+router.get('/signup', (req, res)=>{
+    return res.render('signup')
+})
+
+router.post('/signin', async (req, res)=>{
+    const {email, password} = req.body
+    const user = await User.matchPassword(email,password)
+    console.log(user)
+    return res.redirect("/")
+})
+
+router.post('/signup', async (req, res)=>{
+    const {fullName, email, password} = req.body
+
+    await User.create({
+        fullName,
+        email,
+        password,
+    })
+
+    return res.redirect('home')
+})
+
+module.exports = router
